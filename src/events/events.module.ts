938b5660ab0ca72da7956cfb7987089b33012e53
mongolcontent-tsolmo-b <tsolmon@mongolcontent.mn>
2025-04-15 12:56:31 +0800

    [fix] relation

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -6,9 +6,10 @@ import { EventService } from './events.service';
 import { EventController } from './events.controller';
 import { Organizer } from '../organizers/organizer.entity';
 import { OrganizersService } from '../organizers/organizers.service';
+import { InvitationTemplate } from 'src/invitation-template/invitation-template.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Event, Organizer])],
+  imports: [TypeOrmModule.forFeature([Event, Organizer, InvitationTemplate])],
   providers: [EventService, OrganizersService],
   controllers: [EventController],
 })
