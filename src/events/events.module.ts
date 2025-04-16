cd3799034bf9a52af29f0f5191bc79cf9d177c9c
mongolcontent-tsolmo-b <tsolmon@mongolcontent.mn>
2025-04-16 11:53:25 +0800

    [fix] fix module

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -7,9 +7,13 @@ import { EventController } from './events.controller';
 import { Organizer } from '../organizers/organizer.entity';
 import { OrganizersService } from '../organizers/organizers.service';
 import { InvitationTemplate } from 'src/invitation-template/invitation-template.entity';
+import { EventFormModule } from 'src/event-form/event-form.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Event, Organizer, InvitationTemplate])],
+  imports: [
+    TypeOrmModule.forFeature([Event, Organizer, InvitationTemplate]),
+    EventFormModule
+  ],
   providers: [EventService, OrganizersService],
   controllers: [EventController],
 })
